f3c1f7e7c968b06e2c00bc6d0a1942a62ece0b6e
luke.munday <luke.munday@gmail.com>
2021-06-05 15:25:44 +0100

    Read in each line, seperated by rower and print if line has data

--- src/controllers/fileHandler.py
+++ src/controllers/fileHandler.py
@@ -1,4 +1,5 @@
 from os import walk
+import json
 
 class FileHandler:
     def __init__(self):
@@ -6,7 +7,24 @@ class FileHandler:
         _, _, self.inputFiles = next(walk(self.dataRoot))
 
     def readFile(self):
+        dec = json.JSONDecoder()
+        pos = 0
+
         for file in self.inputFiles:
             with open(self.dataRoot + file) as f:
-                content = f.readlines()
-                print(content)
+                lines = f.readlines()
+                
+                for line in lines:
+                    while not pos == len(str(line))-1:
+                        rowerData, json_len = dec.raw_decode(str(line)[pos:])
+                        pos += json_len
+
+                        if rowerData["info"]["ip"] == "0.0.0.0":
+                            pass
+
+                        print (rowerData)
+                    pos = 0
+                    # break
+                    
+
+                    
