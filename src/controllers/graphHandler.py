090683e819c60cc857580f806cd480095768c1fe
luke.munday <luke.munday@gmail.com>
2021-06-05 17:26:17 +0100

    Added axis labels

--- src/controllers/graphHandler.py
+++ src/controllers/graphHandler.py
@@ -4,19 +4,21 @@ class GraphHandler:
 
     def __init__(self, rowers):
         self.rowers = rowers
-        self.graphMeasurement = ['rx']
+        self.graphMeasurement = 'rx'
 
     def plot(self):
         for rowerIndex in self.rowers:
             rower = self.rowers[rowerIndex]
             rowerGraphData = rower.getGraphData()
             
-            graphData = rowerGraphData['rx']
+            graphData = rowerGraphData[self.graphMeasurement]
 
             readings = graphData.getReadings()
             datetimes = graphData.getDatetimes()
 
             plt.plot(datetimes, readings)
+            plt.ylabel(self.graphMeasurement + ' Rotation (°)')
+            plt.xlabel('Measurement Date Time (dd/mm/yyyy hh:mm:ss)')
             plt.show()
 
             break
